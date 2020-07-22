d5ac841222562c572743b6691f4987dcc0359940
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-07-22 08:39:56 -0300

    Starting battle
    * Modeling dto

---
+++ src/main/java/com/guilherme/aequilibrium/transformers/model/dto/TransformersBattleDTO.java
@@ -0,0 +1,26 @@
+package com.guilherme.aequilibrium.transformers.model.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Data
+@JsonInclude(Include.NON_EMPTY)
+public class TransformersBattleDTO {
+
+    private Integer numberOfBattles;
+    private String winnerTeam;
+    private List<String> losingTeamSurvivors;
+
+}
