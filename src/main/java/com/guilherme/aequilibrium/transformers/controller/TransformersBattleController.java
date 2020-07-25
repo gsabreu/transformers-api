33e84c3c701f75d6f1b13d782de1bc674d1d8135
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-07-25 20:07:47 -0300

    100% tested for service layer

--- src/main/java/com/guilherme/aequilibrium/transformers/controller/TransformersBattleController.java
+++ src/main/java/com/guilherme/aequilibrium/transformers/controller/TransformersBattleController.java
@@ -2,6 +2,8 @@ package com.guilherme.aequilibrium.transformers.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,15 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guilherme.aequilibrium.transformers.model.dto.TransformersBattleResponseDTO;
+import com.guilherme.aequilibrium.transformers.service.TransformersBattleService;
 
 @RestController
 @RequestMapping(path = "/battle")
 public class TransformersBattleController {
 
+    @Autowired
+    private TransformersBattleService transformersBattleService;
+
     @PostMapping
     public ResponseEntity<TransformersBattleResponseDTO> transformersBattle(
 	    @RequestBody(required = true) List<Long> transformersIds) {
-	return null;
+	TransformersBattleResponseDTO response = transformersBattleService.getTeamWinner(transformersIds);
+	return response != null ? new ResponseEntity<>(response, HttpStatus.OK)
+		: new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
---
+++ src/test/java/com/guilherme/aequilibrium/transformers/service/impl/TransformersServiceImplTest.java
@@ -0,0 +1,80 @@
+package com.guilherme.aequilibrium.transformers.service.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.guilherme.aequilibrium.transformers.model.Team;
+import com.guilherme.aequilibrium.transformers.model.TransformerEntity;
+import com.guilherme.aequilibrium.transformers.model.dto.TransformerDTO;
+import com.guilherme.aequilibrium.transformers.repository.TransformersRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TransformersServiceImplTest {
+
+    private final static String NAME = "Optimus Prime";
+
+    @InjectMocks
+    private TransformersServiceImpl service;
+
+    @Mock
+    private TransformersRepository transformersRepository;
+
+    @Before
+    public void setup() {
+
+	TransformerEntity transformerIdOne = TransformerEntity.builder().id(1L).name(NAME).team(Team.AUTOBOTS.acronym)
+		.build();
+
+	when(transformersRepository.save(TransformerEntity.builder().name(NAME).build()))
+		.thenReturn(TransformerEntity.builder().id(1L).name(NAME).build());
+
+	when(transformersRepository.save(transformerIdOne)).thenReturn(transformerIdOne);
+
+	when(transformersRepository.findAll()).thenReturn(Arrays.asList(transformerIdOne));
+    }
+
+    @Test
+    public void should_return_new_transfomers_when_createTransformer() {
+	TransformerDTO result = service.createTransformer(TransformerDTO.builder().name(NAME).build());
+
+	assertNotNull(result);
+	assertThat(result.getId()).isEqualTo(1L);
+    }
+
+    @Test
+    public void should_return_updated_transfomers_when_updateTransformer() {
+	TransformerDTO result = service
+		.updateTransformer(TransformerDTO.builder().id(1L).name(NAME).team(Team.AUTOBOTS.acronym).build());
+
+	assertNotNull(result);
+	assertThat(result.getTeam()).isEqualTo(Team.AUTOBOTS.acronym);
+    }
+
+    @Test
+    public void should_delete_when_deleteTransformer() {
+	service.deleteTransformer(1L);
+
+	verify(transformersRepository, times(1)).deleteById(1L);
+    }
+
+    @Test
+    public void should_return_list_when_getTransformers() {
+	List<TransformerDTO> result = service.getTransformers();
+	assertThat(result).isNotEmpty();
+
+    }
+
+}
